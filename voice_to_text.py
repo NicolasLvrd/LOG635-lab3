0cc52e12931dfe6973fc12a2a35fb987b90b9108
durandnico <nicod4956@gmail.com>
2024-04-25 12:28:44 -0400

    from openai to speechreco

--- voice_to_text.py
+++ voice_to_text.py
@@ -1,6 +1,6 @@
 import sounddevice as sd
 import soundfile as sf
-import whisper
+import speech_recognition as sr
 
 def enregistrer_audio(nom_fichier, duree_enregistrement, frequence_echantillonnage=44100):
     # Enregistrement de l'audio
@@ -13,16 +13,32 @@ def enregistrer_audio(nom_fichier, duree_enregistrement, frequence_echantillonna
     sf.write(nom_fichier, enregistrement, frequence_echantillonnage)
     #print(f"Fichier audio enregistré sous '{nom_fichier}'.")
 
+
 def speech_recognition(nom_fichier):
-    try: 
-        print("\t\033[3;33m#tts : Reconnaissance vocale en cours...\033[0m")
-        model = whisper.load_model("base")
-        result = model.transcribe(nom_fichier, language="french",  fp16=False)
-        print("\t\033[3;33m#tty : Retranscription : ", result["text"])
-        return result["text"]
-    
-    except Exception as e:
-        print(f"Erreur lors de la reconnaissance vocale : {e}")
+
+    # Création d'un objet de reconnaissance vocale
+    reconnaisseur = sr.Recognizer()
+
+    # Ouverture du fichier audio
+    with sr.AudioFile(nom_fichier) as source:
+        audio = reconnaisseur.record(source)
+
+    # Reconnaissance vocale
+    try:
+        texte = reconnaisseur.recognize_google(audio, language="fr-FR")
+        
+        if "Move" in texte:
+            texte = texte.replace("Move", "mauve")
+        
+        print(f"\t\033[3;33m#tts : Texte reconnu : '{texte}'\033[0m")
+        return texte
+    except sr.UnknownValueError:
+        print("\t\033[3;33m#tts : Google Speech Recognition n'a pas pu comprendre l'audio\033[0m")
+    except sr.RequestError as e:
+        print(f"\t\033[3;33m#tts : Erreur lors de la requête à Google Speech Recognition : {e}\033[0m")
+    return None
+
+
 
 if __name__ == "__main__":
     filename = "enregistrement.wav"
